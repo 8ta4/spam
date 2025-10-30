a0612501c3c84f880b40590a59486d4b2086b7f7
a <a@as-Virtual-Machine.local>
2025-10-29 21:23:03 -0700

    Implement get-spreadsheet-id function to extract ID from URL

--- src/main.cljs
+++ src/main.cljs
@@ -1,5 +1,15 @@
 (ns main
-  (:require ["nbb" :refer [loadFile]]))
+  (:require ["nbb" :refer [loadFile]]
+            [clojure.string :refer [split]]
+            [lambdaisland.uri :refer [uri]]))
+
+(defn get-spreadsheet-id
+  [url]
+  (-> url
+      uri
+      :path
+      (split "/")
+      (nth 3)))
 
 (defn init
   [url])
