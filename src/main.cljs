2f5c11dbf099c106ad4eae96566c14f567c74dc4
a <a@as-Virtual-Machine.local>
2025-10-30 15:15:49 -0700

    Connect to spreadsheet on initialization

--- src/main.cljs
+++ src/main.cljs
@@ -1,5 +1,7 @@
 (ns main
   (:require
+   ["google-auth-library" :refer [JWT]]
+   ["google-spreadsheet" :refer [GoogleSpreadsheet]]
    ["nbb" :refer [loadFile]]
    ["os" :refer [homedir]]
    ["path" :refer [join]]
@@ -30,19 +32,26 @@
   (promesa/let [js-config (loadFile "src/bridge.cljs")]
     (reset! config (js->clj js-config :keywordize-keys true))))
 
+(def google-cloud-credentials
+  (-> (homedir)
+      (join ".config/spam/google-cloud.json")
+      slurp
+      js/JSON.parse
+      (js->clj :keywordize-keys true)))
+
+(def service-account-auth
+  (JWT. (clj->js {:email (:client_email google-cloud-credentials)
+                  :key (:private_key google-cloud-credentials)
+                  :scopes ["https://www.googleapis.com/auth/spreadsheets"]})))
+
 (defn init
   [url]
   (initialize-config url)
-  (load-config))
+  (promesa/do (load-config)
+              (promesa/let [spreadsheet (GoogleSpreadsheet. (:spreadsheet @config) service-account-auth)]
+                (.loadInfo spreadsheet))))
 
 (defn main
   [& args]
   (case (first args)
     "init" (init (last args))))
-
-(def google-cloud-credentials
-  (-> (homedir)
-     (join ".config/spam/google-cloud.json")
-     slurp
-     js/JSON.parse
-     (js->clj :keywordize-keys true)))
