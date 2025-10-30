27d13d6ef4aad72105106fb07934a8000dd356f1
a <a@as-Virtual-Machine.local>
2025-10-29 18:07:45 -0700

    Enhance development environment with security and formatting git hooks

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,20 @@
-{ pkgs, lib, config, inputs, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
 
 {
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.gitleaks
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
@@ -41,6 +50,16 @@
 
   # https://devenv.sh/git-hooks/
   # git-hooks.hooks.shellcheck.enable = true;
+  git-hooks.hooks = {
+    gitleaks = {
+      enable = true;
+      # https://github.com/gitleaks/gitleaks/blob/ed65b65095eb699c614aa5ac058f6724539fb03f/.pre-commit-hooks.yaml#L4
+      # Direct execution of gitleaks here results in '[git] fatal: cannot change to 'devenv.nix': Not a directory'.
+      entry = "bash -c 'exec gitleaks git --redact --staged --verbose'";
+    };
+    # https://github.com/NixOS/nixfmt/blob/2caa09642c3cde5985cf8d239ffc66094c344c57/README.md?plain=1#L168
+    nixfmt-rfc-style.enable = true;
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
