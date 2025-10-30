80a9d0e3499d13385b5a686c738da34e58a25cbb
a <a@as-Virtual-Machine.local>
2025-10-29 21:12:55 -0700

    Add init function with URL parameter placeholder

--- src/main.cljs
+++ src/main.cljs
@@ -1,8 +1,13 @@
 (ns main
   (:require ["nbb" :refer [loadFile]]))
 
+(defn init
+  [url])
+
 (defn main
-  [])
+  [& args]
+  (case (first args)
+    "init" (init (last args))))
 
 (defn load-config
   []
