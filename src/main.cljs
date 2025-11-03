f08769182f5f085ad2e1fe5ac43b8a644cc9ae8b
a <a@as-Virtual-Machine.local>
2025-11-03 01:07:08 -0800

    Add see activity definition and register it with worker

--- src/main.cljs
+++ src/main.cljs
@@ -181,10 +181,14 @@
          (map (comp prepare-contexts :endpoint))
          clj->js)))
 
+(defn see
+  [source])
+
 (defstate worker
 ; https://github.com/tolitius/mount/issues/118#issuecomment-667433275
   :start (let [worker* (atom nil)]
-           (promesa/let [worker** (.create Worker (clj->js {:activities (clj->js {:orchestrate orchestrate})
+           (promesa/let [worker** (.create Worker (clj->js {:activities (clj->js {:orchestrate orchestrate
+                                                                                  :see see})
                                                             :taskQueue task-queue
                                                             :workflowsPath (path/join (toString) "target/workflows.js")}))]
              (reset! worker* worker**)
--- src/workflows.cljs
+++ src/workflows.cljs
@@ -1,7 +1,7 @@
 (ns workflows
   (:require
    ["@temporalio/workflow" :refer [proxyActivities]]
-   [promesa.core :as promesa]))
+   [promesa.core :as promesa :refer [all]]))
 
 (def activities
   (proxyActivities (clj->js {:startToCloseTimeout (* 60 1000)})))
@@ -9,7 +9,9 @@
 (defn spam
   []
   (promesa/let [data (.orchestrate activities)]
-    (->> (js->clj data :keywordize-keys true)
-         (mapcat :sources)
-         distinct
-         clj->js)))
+    (promesa/->> (js->clj data :keywordize-keys true)
+                 (mapcat :sources)
+                 distinct
+                 (map #(.see activities (clj->js %)))
+                 all
+                 clj->js)))
