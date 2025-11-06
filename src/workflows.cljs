b7821e3cfdf496743954f8f8119ede0e4003d813
a <a@as-Virtual-Machine.local>
2025-11-06 14:09:35 -0800

    Improve workflows orchestration by applying source-content mapping

--- src/workflows.cljs
+++ src/workflows.cljs
@@ -1,6 +1,7 @@
 (ns workflows
   (:require
    ["@temporalio/workflow" :refer [proxyActivities]]
+   [com.rpl.specter :refer [ALL transform]]
    [promesa.core :as promesa :refer [all]]))
 
 (def activities
@@ -11,9 +12,9 @@
   (promesa/let [data (.orchestrate activities)
                 sources (->> (js->clj data :keywordize-keys true)
                              (mapcat :sources)
-                             distinct)]
-    (promesa/->> sources
-                 (map #(.see activities (clj->js %)))
-                 all
-                 (zipmap sources)
-                 clj->js)))
+                             distinct)
+                source-content (promesa/->> sources
+                                            (map #(.see activities (clj->js %)))
+                                            all
+                                            (zipmap sources))]
+    (clj->js (transform [ALL :sources ALL] source-content (js->clj data :keywordize-keys true)))))
