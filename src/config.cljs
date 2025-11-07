c71659877226b014a177c60718a4d8d09a9dcc52
a <a@as-Virtual-Machine.local>
2025-11-07 00:28:24 -0800

    Add challenger configuration with user and system fields

--- src/config.cljs
+++ src/config.cljs
@@ -4,4 +4,7 @@
                      :system "You are a world-class sales copywriter specializing in hyper-personalized, concise cold outreach. Your task is to draft a single, compelling message based on the context provided."}
            :judge {:user (fn [{:keys [date endpoint messages sources a b]}]
                            (str date endpoint messages sources a b))
-                   :system ""}}}
+                   :system ""}
+           :challenger {:user (fn [{:keys [date endpoint messages sources a b winner critique]}]
+                                (str date endpoint messages sources a b winner critique))
+                        :system ""}}}
