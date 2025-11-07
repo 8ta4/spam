f4c4770790ba5e671aa599cc6243079385f02e74
a <a@as-Virtual-Machine.local>
2025-11-06 23:35:37 -0800

    Add judge prompt configuration with user and system fields

--- src/config.cljs
+++ src/config.cljs
@@ -1,4 +1,7 @@
 {:spreadsheet "<spreadsheet-id>"
  :prompts {:creator {:user (fn [{:keys [date endpoint messages sources]}]
                              (str date endpoint messages sources))
-                     :system "You are a world-class sales copywriter specializing in hyper-personalized, concise cold outreach. Your task is to draft a single, compelling message based on the context provided."}}}
+                     :system "You are a world-class sales copywriter specializing in hyper-personalized, concise cold outreach. Your task is to draft a single, compelling message based on the context provided."}
+           :judge {:user (fn [{:keys [date endpoint messages sources a b]}]
+                           (str date endpoint messages sources a b))
+                   :system ""}}}
